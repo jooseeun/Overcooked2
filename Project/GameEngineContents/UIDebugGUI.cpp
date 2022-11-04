cc7406b610fcc1d24fbbca80c2ab5a59d9f56505
floorbear2723 <floorbear2723@gmail.com>
2022-11-04 19:23:39 +0900

    이동원 : UI업데이트

---
+++ Project/GameEngineContents/UIDebugGUI.cpp
@@ -0,0 +1,169 @@
+#include "PreCompile.h"
+#include "UIDebugGUI.h"
+
+#include <GameEngineCore/GameEngineCamera.h>
+
+std::map<std::string, float> UIDebugGUI::DebugValue_float_;
+std::map<std::string, int> UIDebugGUI::DebugValue_int_;
+std::map<std::string, float4> UIDebugGUI::DebugValue_float4_;
+std::map<std::string, std::string> UIDebugGUI::DebugValue_string_;
+std::map<std::string, float*> UIDebugGUI::MutableValue_float_;
+std::map<std::string, int*> UIDebugGUI::MutableValue_int_;
+std::map<std::string, float4*> UIDebugGUI::MutableValue_float4_;
+std::map<std::string, GameEngineTransform*> UIDebugGUI::Transform_;
+
+UIDebugGUI* UIDebugGUI::Main_ = nullptr;
+
+UIDebugGUI::UIDebugGUI()
+{
+}
+
+UIDebugGUI::~UIDebugGUI()
+{
+}
+
+void UIDebugGUI::Initialize(GameEngineLevel* _Level)
+{
+}
+
+void UIDebugGUI::OnGUI(GameEngineLevel* _Level, float _DeltaTime)
+{
+	//DebugValue_int
+	if (DebugValue_int_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("DebugValue int", 0) == true)
+		{
+			for (auto i : DebugValue_int_)
+			{
+				std::string Text = i.first + " : " + std::to_string((i.second));
+				ImGui::Text(Text.c_str());
+			}
+			//업데이트를 위해서
+			DebugValue_int_.clear();
+			ImGui::TreePop();
+		}
+	}
+
+	//DebugValue_float
+	if (DebugValue_float_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("DebugValue Float", 0) == true)
+		{
+			for (auto i : DebugValue_float_)
+			{
+				std::string Text = i.first + " : " + std::to_string((i.second));
+				ImGui::Text(Text.c_str());
+			}
+			//업데이트를 위해서
+			DebugValue_float_.clear();
+			ImGui::TreePop();
+		}
+	}
+
+	//DebugValue_string
+	if (DebugValue_string_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("DebugValue String", 0) == true)
+		{
+			for (auto i : DebugValue_string_)
+			{
+				std::string Text = i.first + " : " + (i.second);
+				ImGui::Text(Text.c_str());
+			}
+			DebugValue_string_.clear();
+			ImGui::TreePop();
+		}
+	}
+
+	//DebugValue_float
+	if (DebugValue_float4_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("DebugValue Float4", 0) == true)
+		{
+			for (auto i : DebugValue_float4_)
+			{
+				std::string Text = i.first + "\nX : " + std::to_string((i.second.x)) + "\nY : " + std::to_string((i.second.y)) + "\nZ : " + std::to_string((i.second.z));
+				ImGui::Text(Text.c_str());
+			}
+			//업데이트를 위해서
+			DebugValue_float4_.clear();
+			ImGui::TreePop();
+		}
+	}
+
+	//MutableValue_float
+	if (MutableValue_float_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("MutableValue_float_", 0) == true)
+		{
+			for (auto i : MutableValue_float_)
+			{
+				ImGui::InputFloat(i.first.c_str(), i.second);
+			}
+			ImGui::TreePop();
+		}
+	}
+
+	//MutableValue_int
+	if (MutableValue_int_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("MutableValue_Int_", 0) == true)
+		{
+			for (auto i : MutableValue_int_)
+			{
+				ImGui::InputInt(i.first.c_str(), i.second);
+			}
+			ImGui::TreePop();
+		}
+	}
+
+	//MutableValue_float4
+	if (MutableValue_float4_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("MutableValue_float4_", 0) == true)
+		{
+			for (auto i : MutableValue_float4_)
+			{
+				ImGui::InputFloat4(i.first.c_str(), i.second->Arr1D);
+			}
+			ImGui::TreePop();
+		}
+	}
+
+	//Transform
+	if (Transform_.empty() == false)
+	{
+		if (ImGui::TreeNodeEx("Transform", 0) == true)
+		{
+			for (auto i : Transform_)
+			{
+				if (ImGui::TreeNodeEx(i.first.c_str(), 0) == true)
+				{
+					//로컬 포지션
+					float4 LocalPos = i.second->GetLocalPosition();
+					ImGui::InputFloat4("LocalPos", LocalPos.Arr1D);
+					i.second->SetLocalPosition(LocalPos);
+					//로컬 스케일
+					float4 LocalScale = i.second->GetLocalScale();
+					ImGui::InputFloat4("LocalScale", LocalScale.Arr1D);
+					i.second->SetLocalScale(LocalScale);
+					//로컬 로테이션
+					float4 LocalRot = i.second->GetLocalRotation();
+					ImGui::InputFloat4("LocalRot", LocalRot.Arr1D);
+					i.second->SetLocalRotation(LocalRot);
+
+					//월드 포지션
+					float4 WorldPos = i.second->GetWorldPosition();
+					ImGui::InputFloat4("WorldPos", WorldPos.Arr1D);
+					i.second->SetWorldPosition(WorldPos);
+					//월드 스케일
+					float4 WorldScale = i.second->GetWorldScale();
+					ImGui::InputFloat4("WorldScale", WorldScale.Arr1D);
+					i.second->SetWorldScale(WorldScale);
+					ImGui::TreePop();
+				}
+			}
+			ImGui::TreePop();
+		}
+	}
+}
---
+++ Project/GameEngineContents/UIDebugGUI.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <GameEngineCore/GameEngineGUI.h>
+#include <GameEngineBase/GameEngineMath.h>
+
+class UIDebugGUI : public GameEngineGUIWindow
+{
+public:
+	UIDebugGUI();
+	~UIDebugGUI();
+
+	UIDebugGUI(const UIDebugGUI& _Other) = delete;
+	UIDebugGUI(const UIDebugGUI&& _Other) noexcept = delete;
+	UIDebugGUI& operator=(const UIDebugGUI& _Ohter) = delete;
+	UIDebugGUI& operator=(const UIDebugGUI&& _Other) noexcept = delete;
+
+	static void AddValue(const std::string _Name, int _Value)
+	{
+		DebugValue_int_.insert(std::make_pair(_Name, _Value));
+	}
+	static void AddValue(const std::string _Name, float _Value)
+	{
+		DebugValue_float_.insert(std::make_pair(_Name, _Value));
+	}
+	static void AddValue(const std::string _Name, float4 _Value)
+	{
+		DebugValue_float4_.insert(std::make_pair(_Name, _Value));
+	}
+	static void AddValue(const std::string _Name, std::string _Value)
+	{
+		DebugValue_string_.insert(std::make_pair(_Name, _Value));
+	}
+	static void AddMutableValue(const std::string _Name, float* _Value)
+	{
+		MutableValue_float_.insert(std::make_pair(_Name, _Value));
+	}
+
+	static void AddMutableValue(const std::string _Name, int* _Value)
+	{
+		MutableValue_int_.insert(std::make_pair(_Name, _Value));
+	}
+
+	static void AddMutableValue(const std::string _Name, float4* _Value)
+	{
+		MutableValue_float4_.insert(std::make_pair(_Name, _Value));
+	}
+
+	static void AddTransform(const std::string _Name, GameEngineTransform* _Value)
+	{
+		Transform_.insert(std::make_pair(_Name, _Value));
+	}
+
+private:
+	void Initialize(class GameEngineLevel* _Level) override;
+	void OnGUI(GameEngineLevel* _Level, float _DeltaTime) override;
+
+	static std::map<std::string, int> DebugValue_int_;
+	static std::map<std::string, float> DebugValue_float_;
+	static std::map<std::string, float4> DebugValue_float4_;
+	static std::map<std::string, std::string> DebugValue_string_;
+
+	static std::map<std::string, float*> MutableValue_float_;
+	static std::map<std::string, int*> MutableValue_int_;
+	static std::map<std::string, float4*> MutableValue_float4_;
+
+	static std::map<std::string, GameEngineTransform*> Transform_;
+
+public:
+	static UIDebugGUI* Main_;
+};
