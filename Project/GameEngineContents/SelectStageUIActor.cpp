d5b6ca197bc33bc8cfae56ad10dffa9b443c9f2e
floorbear2723 <floorbear2723@gmail.com>
2022-11-06 17:55:57 +0900

    이동원 : 폰트적용

--- Project/GameEngineContents/SelectStageUIActor.cpp
+++ Project/GameEngineContents/SelectStageUIActor.cpp
@@ -1,6 +1,9 @@
 #include "PreCompile.h"
 #include "SelectStageUIActor.h"
 #include "OverCookedUIRenderer.h"
+#include "UIDebugGUI.h"
+#include <GameEngineCore/GameEngineFont.h>
+#include <GameEngineCore/GameEngineFontRenderer.h>
 
 SelectStageUIActor::SelectStageUIActor()
 {
@@ -29,6 +32,8 @@ void SelectStageUIActor::Start()
 		}
 	}
 
+	GameEngineFont::Load("Naughty Squirrel");
+
 	EndBackground_ = CreateComponent<OverCookedUIRenderer>("EndBackground");
 	EndBackground_->SetTexture("EndBackground.png");
 	EndBackground_->ScaleToTexture();
@@ -41,6 +46,12 @@ void SelectStageUIActor::Start()
 	Header_->SetTexture("t_ui_dialog_header_01_d.png");
 	Header_->ScaleToTexture();
 	Header_->GetTransform().SetLocalPosition({ -260,290 });
+
+	CountDownFont_ = CreateComponent<GameEngineFontRenderer>("CowntDownFont");
+	CountDownFont_->ChangeCamera(CAMERAORDER::UICAMERA);
+	CountDownFont_->SetText("0sd4", "Naughty Squirrel");
+	CountDownFont_->SetColor({ 120.f/256.f,178.f/256.f,218.f/256.f,1 });
+	CountDownFont_->SetPositionMode(FontPositionMode::SCREEN);
 }
 
 void SelectStageUIActor::Update(float _DeltaTime)
