cd0a60fa200b13204c1b930a4f2005f064298de8
floorbear2723 <floorbear2723@gmail.com>
2022-11-07 18:39:33 +0900

    이도우언 : 방향키를 통한 맵 선택 로직 구현중

--- Project/GameEngineContents/SelectStageUIActor.cpp
+++ Project/GameEngineContents/SelectStageUIActor.cpp
@@ -15,6 +15,88 @@ SelectStageUIActor::~SelectStageUIActor()
 
 void SelectStageUIActor::Start()
 {
+	LoadResource();
+	InitRenderer();
+
+	//CountDownFont_ = CreateComponent<GameEngineFontRenderer>("CowntDownFont");
+	//CountDownFont_->ChangeCamera(CAMERAORDER::UICAMERA);
+	//CountDownFont_->SetText("0sd4", "Naughty Squirrel");
+	//CountDownFont_->SetColor({ 120.f / 256.f,178.f / 256.f,218.f / 256.f,1 });
+	//CountDownFont_->SetSize(30.f);
+	//CountDownFont_->SetPositionMode(FontPositionMode::SCREEN);
+}
+
+void SelectStageUIActor::InitRenderer()
+{
+	EndBackground_ = CreateUIRenderer("EndBackground.png");
+
+	Background_ = CreateUIRenderer("UI_PauseScreen_Backdrop_01.png");
+
+	Header_ = CreateUIRenderer("t_ui_dialog_header_01_d.png");
+	Header_->GetTransform().SetLocalPosition({ -260,290 });
+
+	ControlGuide_ = CreateUIRenderer("control_bg.png");
+	ControlGuide_->GetTransform().SetLocalPosition({ -318,-308 });
+
+	SelectMap_ = CreateUIRenderer("Selectmap.png");
+	SelectMap_->GetTransform().SetLocalPosition({ 20,-30 });
+
+	{
+		//패널
+		LevelSelect NewSelect;
+
+		//부모
+		OverCookedUIRenderer* NewParent = CreateUIRenderer("AvatarSelectionRing.png");
+		NewParent->GetTransform().SetLocalScale({ 1,1,1 });
+		NewParent->GetTransform().SetLocalPosition({ 0,130,-10 });
+		NewParent->ResistDebug();
+		NewParent->Off();
+		NewSelect.Parent = NewParent;
+
+		//맵 이미지
+		OverCookedUIRenderer* NewRenderer = CreateUIRenderer("level_select_sushi.png");
+		NewRenderer->GetTransform().SetParentTransform(NewParent->GetTransform());
+		NewSelect.Select = NewRenderer;
+
+		NewSelect.BoarderType = 1;
+
+		//Non Selected Boarder
+		OverCookedUIRenderer* NewBoarder = CreateUIRenderer("level_boarder_0" + std::to_string(NewSelect.BoarderType) + "_not_selected.png");
+		NewBoarder->GetTransform().SetParentTransform(NewParent->GetTransform());
+		NewBoarder->GetTransform().SetLocalMove({ 0,0,-1 });
+		NewSelect.Boarder = NewBoarder;
+
+		//Select Boarder
+		OverCookedUIRenderer* NewSelectBoarder = CreateUIRenderer("level_boarder_0" + std::to_string(NewSelect.BoarderType) + "_selected.png");
+		NewSelectBoarder->GetTransform().SetParentTransform(NewParent->GetTransform());
+		NewSelectBoarder->GetTransform().SetLocalMove({ 0,0,-1 });
+		NewSelectBoarder->Off();
+		NewSelect.BoarderSelected = NewSelectBoarder;
+
+		//레벨하이라이트
+		OverCookedUIRenderer* LevelHighlight = CreateUIRenderer("level_highlight.png");
+		LevelHighlight->GetTransform().SetParentTransform(NewParent->GetTransform());
+		LevelHighlight->GetTransform().SetLocalMove({ 0,0,-1 });
+		LevelHighlight->Off();
+		NewSelect.LevelHighlisht = LevelHighlight;
+
+		//폰트
+		GameEngineFontRenderer* NewFont = CreateComponent<GameEngineFontRenderer>("SushiCity");
+		NewFont->ChangeCamera(CAMERAORDER::UICAMERA);
+		NewFont->SetText("초밥 시티", "Naughty Squirrel");
+		NewFont->SetColor({ 0.4f,0.4f,0.4f,1 });
+		NewFont->SetSize(20.f);
+		NewFont->SetLeftAndRightSort(LeftAndRightSort::CENTER);
+		NewFont->SetAffectTransform(true);
+		NewFont->GetTransform().SetLocalMove({ 0,-65.f,-1 });
+		NewFont->GetTransform().SetParentTransform(NewParent->GetTransform());
+
+		LevelSelect_.push_back(NewSelect);
+	}
+}
+
+void SelectStageUIActor::LoadResource()
+{
 	//필요한 리소스 로드
 	{
 		GameEngineDirectory Dir;
@@ -33,31 +115,29 @@ void SelectStageUIActor::Start()
 	}
 
 	GameEngineFont::Load("Naughty Squirrel");
-
-	EndBackground_ = CreateComponent<OverCookedUIRenderer>("EndBackground");
-	EndBackground_->SetTexture("EndBackground.png");
-	EndBackground_->ScaleToTexture();
-
-	Background_ = CreateComponent<OverCookedUIRenderer>("Background");
-	Background_->SetTexture("UI_PauseScreen_Backdrop_01.png");
-	Background_->ScaleToTexture();
-
-	Header_ = CreateComponent<OverCookedUIRenderer>("Header");
-	Header_->SetTexture("t_ui_dialog_header_01_d.png");
-	Header_->ScaleToTexture();
-	Header_->GetTransform().SetLocalPosition({ -260,290 });
-
-	CountDownFont_ = CreateComponent<GameEngineFontRenderer>("CowntDownFont");
-	CountDownFont_->ChangeCamera(CAMERAORDER::UICAMERA);
-	CountDownFont_->SetText("0sd4", "Naughty Squirrel");
-	CountDownFont_->SetColor({ 120.f/256.f,178.f/256.f,218.f/256.f,1 });
-	CountDownFont_->SetPositionMode(FontPositionMode::SCREEN);
 }
 
 void SelectStageUIActor::Update(float _DeltaTime)
 {
+	if (GameEngineInput::GetInst()->IsDownKey("PlayerLeft") == true)
+	{
+		LevelSelect_[0].Parent->GetTransform().SetLocalMove(float4::RIGHT * 10.0f);
+	}
 }
 
 void SelectStageUIActor::End()
 {
+}
+
+void SelectStageUIActor::ResistDebug(std::string_view _Name, GameEngineTransform& Trans)
+{
+	UIDebugGUI::Main_->AddTransform(_Name.data(), &Trans);
+}
+
+OverCookedUIRenderer* SelectStageUIActor::CreateUIRenderer(std::string_view _TextrueName)
+{
+	OverCookedUIRenderer* NewRenderer = CreateComponent<OverCookedUIRenderer>(_TextrueName.data());
+	NewRenderer->SetTexture(_TextrueName.data());
+	NewRenderer->ScaleToTexture();
+	return NewRenderer;
 }
--- Project/GameEngineContents/SelectStageUIActor.h
+++ Project/GameEngineContents/SelectStageUIActor.h
@@ -3,6 +3,16 @@
 class OverCookedUIRenderer;
 class SelectStageUIActor : public GameEngineActor
 {
+	struct LevelSelect
+	{
+		OverCookedUIRenderer* Parent;
+		OverCookedUIRenderer* Select;
+		GameEngineFontRenderer* Font;
+		int BoarderType; //1,2,3
+		OverCookedUIRenderer* Boarder;
+		OverCookedUIRenderer* BoarderSelected;
+		OverCookedUIRenderer* LevelHighlisht;
+	};
 public:
 	SelectStageUIActor();
 	~SelectStageUIActor();
@@ -14,15 +24,25 @@ public:
 
 protected:
 	void Start() override;
+	void InitRenderer();
+	void LoadResource();
 	void Update(float _DeltaTime) override;
 	void End() override;
+
+	void ResistDebug(std::string_view _Name, GameEngineTransform& Trans);
+
+	OverCookedUIRenderer* CreateUIRenderer(std::string_view _TextrueName);
 
 private:
 	OverCookedUIRenderer* EndBackground_ = nullptr;
 	OverCookedUIRenderer* Background_ = nullptr;
 	OverCookedUIRenderer* Header_ = nullptr;
+	OverCookedUIRenderer* ControlGuide_ = nullptr;
+	OverCookedUIRenderer* SelectMap_ = nullptr;
 
 	GameEngineFontRenderer* CountDownFont_ = nullptr;
+
+	std::vector<LevelSelect> LevelSelect_;
 
 	float4 DebugPos_;
 	float DebugSize_;
