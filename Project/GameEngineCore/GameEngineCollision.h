80ec8aaa8024bc6835a22ce3cfc0a2c661ceddfe
wndgur98@gmail.com <wndgur98@gmail.com>
2022-11-07 14:47:31 +0900

    장중혁 : ExitBase 순서 수정

--- Project/GameEngineCore/GameEngineCollision.h
+++ Project/GameEngineCore/GameEngineCollision.h
@@ -77,9 +77,9 @@ public:
 
 	bool IsCollisionExitBase(CollisionType _ThisType, int _GroupOrder
 		, CollisionType _OtherType
-		, std::function<CollisionReturn(GameEngineCollision* _This, GameEngineCollision* _Other)> _Enter = nullptr
-		, std::function<CollisionReturn(GameEngineCollision* _This, GameEngineCollision* _Other)> _Update = nullptr
 		, std::function<CollisionReturn(GameEngineCollision* _This, GameEngineCollision* _Other)> _Exit = nullptr
+		, std::function<CollisionReturn(GameEngineCollision* _This, GameEngineCollision* _Other)> _Update = nullptr
+		, std::function<CollisionReturn(GameEngineCollision* _This, GameEngineCollision* _Other)> _Enter = nullptr
 	) 
 	{
 		return IsCollision(_ThisType, _GroupOrder, _OtherType, _Update, _Enter, _Exit);
