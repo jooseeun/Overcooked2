2c7f899ba8a26d3a7c0bd78c0c94731fa382874f
이삭 <alwlsdydtjd1@gmail.com>
2022-11-07 18:04:30 +0900

    이삭: 서비스 해치 콜리젼 크기 수정

--- Project/GameEngineContents/Servicehatch.cpp
+++ Project/GameEngineContents/Servicehatch.cpp
@@ -13,8 +13,8 @@ void Servicehatch::Start()
 {
 	GamePlayStaticObject::Start();
 
-	GetCollisionObject()->GetTransform().SetWorldScale({ 200, 50, 200 });
-	GetCollisionObject()->GetTransform().SetWorldMove({ -60, 25, 0 });
+	GetCollisionObject()->GetTransform().SetWorldScale({ 230, 50, 150 });
+	GetCollisionObject()->GetTransform().SetWorldMove({ -60, 25, -10 });
 
 	GetFBXMesh()->SetFBXMesh("m_dlc08_servicehatch_01.fbx", "Texture");
 	GetFBXMesh()->GetTransform().SetWorldScale({ 100, 100, 100 });
