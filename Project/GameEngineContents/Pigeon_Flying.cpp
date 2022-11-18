859031c942449201b3ff04ec6d050a007bc6a12f
Yun seo <joypen1267@naver.com>
2022-11-18 15:40:30 +0900

    이윤서: 애니메이션 수정

--- Project/GameEngineContents/Pigeon_Flying.cpp
+++ Project/GameEngineContents/Pigeon_Flying.cpp
@@ -14,8 +14,8 @@ void Pigeon_Flying::Start()
 	GamePlayMapObject::Start();
 	GetAnimationFBXMesh()->GetTransform().SetWorldScale({ 140.f, 140.f, 140.f });
 	GetAnimationFBXMesh()->SetFBXMesh("m_city_pigeon_01.fbx", "TextureAnimation", 0);	// Flying
-	GetAnimationFBXMesh()->CreateFBXAnimation("Fly", {});
-	GetAnimationFBXMesh()->ChangeAnimation("Fly");
+	GetAnimationFBXMesh()->CreateFBXAnimation("Move", GameEngineRenderingEvent("m_city_pigeon_01.fbx", 0.04f, true));
+	GetAnimationFBXMesh()->ChangeAnimation("Move");
 	GetCollisionObject()->Off();
 }
 
