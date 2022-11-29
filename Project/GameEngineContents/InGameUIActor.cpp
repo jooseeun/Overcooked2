0006001a7a057c0e9841b45b2f3b1ffebf5ba868
floorbear2723 <floorbear2723@gmail.com>
2022-11-29 19:45:35 +0900

    이동원 : 릭나는거 수정

--- Project/GameEngineContents/InGameUIActor.cpp
+++ Project/GameEngineContents/InGameUIActor.cpp
@@ -76,10 +76,10 @@ void InGameUIActor::UIStart()
 	}
 
 	//레시피 매니저
-	RecipeManager_.Init(std::dynamic_pointer_cast<InGameUIActor>(shared_from_this()));
+	/*RecipeManager_.Init(std::dynamic_pointer_cast<InGameUIActor>(shared_from_this()));
 	RecipeManager_.CreateRecipe(FoodType::FishSushimi);
 	RecipeManager_.CreateRecipe(FoodType::CheeseBurger);
-	RecipeManager_.CreateRecipe(FoodType::CheeseBurgerLettuceTomato);
+	RecipeManager_.CreateRecipe(FoodType::CheeseBurgerLettuceTomato);*/
 }
 
 void InGameUIActor::UIUpdate(float _DeltaTime)
@@ -119,7 +119,7 @@ void InGameUIActor::UIUpdate(float _DeltaTime)
 		}
 	}
 
-	RecipeManager_.Update(_DeltaTime);
+	/*RecipeManager_.Update(_DeltaTime);*/
 
 	//UpdateScore
 	UpdateTime(_DeltaTime);
