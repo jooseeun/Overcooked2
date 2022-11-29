1b9be684634a0e3788d124af988437c7f01d7fef
joo5484@gmail.com <joo5484@gmail.com>
2022-11-29 17:26:05 +0900

    주세은 : throw함수 추가, 플레이어 null 검사 추가

--- Project/GameEngineContents/GamePlayPhysics.cpp
+++ Project/GameEngineContents/GamePlayPhysics.cpp
@@ -22,12 +22,26 @@ void GamePlayPhysics::Gravity()
 }
 void GamePlayPhysics::ThrowUpdate()
 {
-
+	
 }
 void GamePlayPhysics::Throw(float4 _Vector)
 {
+	if (IsThrow_ == false)
+	{
+		IsThrow_ = true;
+		UpTime_ = 1.0f;
+	}
+	else
+	{
+		if (UpTime_ > 0.0f)
+		{
+			UpTime_ -= 1.0f * GameEngineTime::GetDeltaTime();
+			GetTransform().SetWorldUpMove(100.0f, GameEngineTime::GetDeltaTime());
+		}
 
-	GetTransform().SetWorldMove(_Vector * GameEngineTime::GetDeltaTime() * 100.0f);
+		GetTransform().SetWorldMove(_Vector * GameEngineTime::GetDeltaTime() * 100.0f);
+	}
+	
 }
 
 bool GamePlayPhysics::StaticObjectCollisionCheck() // 벽, 맵 오브젝트랑 충돌하면 true 반환, 충돌안하면 false 반환
